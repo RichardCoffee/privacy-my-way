51551ef2a81ff55a024bbbcbe1c3f6d694fc0970
Laptop 3 <richard.coffee@gmail.com>
2018-05-04 02:40:47 -0400

    logging

--- classes/privacy.php
+++ classes/privacy.php
@@ -457,11 +457,11 @@ pmw(1)->log( pmw()->get_calling_function(), $check, $trans );
 
 	public function log_filter_arguments() {
 		$args = func_get_args();
-pmw(1)->log( pmw()->get_calling_function(), $args );
+pmw(1)->log( pmw()->get_calling_function(), current_filter(), $args );
 if ( ( isset( $args[0]->response ) && isset( $args[0]->response['foogallery/foogallery.php'] ) )
 	|| ( isset( $args[0]->checked ) && isset( $args[0]->checked['foogallery/foogallery.php'] ) )
 	|| ( isset( $args[2] ) && ( $args[2] === 'plugin_slugs' ) ) ) {
-  pmw(1)->log('stack');
+  pmw(1)->log(0,'stack');
 }
 		return $args[0];
 	}
